7511dcd5578fcae0ef6627e6fd1f77c6b8bb7144
bergsportprofi <bergsportprofi.net@surrealnetworks.io>
2022-12-16 17:52:39 +0100

    minor change

--- components/Products.vue
+++ components/Products.vue
@@ -2,7 +2,7 @@
   <div class="row display-flex">
     <div
       v-for="(product, index) in products"
-      :key="index  + Math.random()"
+      :key="index"
       class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mt-5 fadeInUp"
     >
       <ProductItem-v2 :product="product" />
